11a47f17e52ff5181887dfc9e5be14df14473016
Zack Falgout <zfalgout@gmail.com>
2017-05-16 06:24:04 -0500

    Started chapter 18

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Zack Falgout on 5/16/17.
+//  Copyright © 2017 Zack Falgout. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
